f9d853ae160b9d76390b54543bf2b782fe158d47
mostlyaman <amansharma110603@gmail.com>
2023-04-12 19:28:16 +0530

    test face_search

--- Web/app.py
+++ Web/app.py
@@ -1,1 +1,8 @@
 from flask_app import app
+from flask import send_from_directory
+import os
+
+# Only used in development and not in 
+@app.route("/<path:path>", methods=['GET'])
+def serve_static_files(path):
+    return send_from_directory('assets', path)
--- Web/flask_app.py
+++ Web/flask_app.py
@@ -1,5 +1,8 @@
 from flask import Flask
 from flask import request
+from model import face_search
+import cv2
+import numpy as np
 
 import json
 
@@ -12,9 +15,14 @@ def test_server():
     return "Server is running."
 
 
-@app.route('/api/v1/face_recognition', methods=['GET'])
+@app.route('/api/v1/face_recognition', methods=['POST'])
 def face_recognition():
     try:
-        return json.dumps({"status":"ok"}), 200
+        if 'image' in request.files:
+            image = request.files['image']
+            frame = cv2.imdecode(np.frombuffer(image.read(), np.uint8), cv2.IMREAD_COLOR)
+            return json.dumps({"status": "ok", "result": face_search(frame)})
+        else:
+            return json.dumps({"status":"error", "message": "No Image Selected."}), 400
     except Exception as e:
-        return json.dumps({"status":"error", "message":"Something went wrong."}), 500
+        return json.dumps({"status":"error", "message":"Something went wrong.", "error": str(e)}), 500
